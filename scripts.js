c15919887d9b3611e7980e90aba77c770bccb5eb
Dominika Zdunek <dozdunek@gmail.com>
2017-10-08 19:46:31 +0200

    poprawione

--- scripts.js
+++ scripts.js
@@ -2,5 +2,5 @@ var text = "Velociraptor is a genus of herbivorous ceratopsid dinosaur that firs
 var dinosaur = "triceratops";
 var dinosaurUppercase = dinosaur.toUpperCase();
 var changendSentence = text.replace('Velociraptor', dinosaurUppercase);
-console.log(text.length);
-console.log(changendSentence.slice(0,72));
+var textLength = text.length;
+console.log(changendSentence.slice(0,textLength/2));
